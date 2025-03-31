3f746e0ef8192acecc0b12c35a85489076574c79
Grace Fowler <f.llfr.mgrace@gmail.com>
2025-03-30 22:52:57 -0400

    Guh

--- 314-ASMT-W8-Unity/Assets/Scripts/DoorInteractable.cs
+++ 314-ASMT-W8-Unity/Assets/Scripts/DoorInteractable.cs
@@ -10,51 +10,19 @@ public class DoorInteractable : SimpleHingeInteractable
     [SerializeField] Transform doorObject;
     [SerializeField] CombinationLock combolock;
 
-    [SerializeField] bool isRightSide;
-
-    float rotation;
+    [SerializeField] float minAngle;
+    [SerializeField] float maxAngle;
 
     // Start is called before the first frame update
     void Start()
     {
-        rotation = transform.rotation.y;
+
     }
 
     protected override void Update()
     {
-        if (combolock != null)
-        {
-            if (!combolock.isLocked)
-            {
-                MoveHinge();
-            }
-        }
-        else
-        {
-            MoveHinge();
-        }
-        if (isRightSide)
-        {
-            transform.localEulerAngles = new Vector3(0, Mathf.Clamp(transform.localRotation.eulerAngles.y, 0, 90), 0);
-        }
-        else
-        {
-            transform.localEulerAngles = new Vector3(0, Mathf.Clamp(transform.localRotation.eulerAngles.y, 270, 360 - Mathf.Epsilon), 0);
-        }
-    }
-
-    private void MoveHinge()
-    {
         base.Update();
 
-        if (doorObject != null)
-        {
-            doorObject.localEulerAngles = new Vector3
-                (
-                doorObject.localEulerAngles.x,
-                transform.localEulerAngles.y,
-                doorObject.localEulerAngles.z
-                );
-        }
+        transform.localEulerAngles = new Vector3(0, Mathf.Clamp(transform.localRotation.eulerAngles.y, minAngle, maxAngle), 0);
     }
 }
